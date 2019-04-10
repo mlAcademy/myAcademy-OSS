73101a3de1b8d26beb6f2d7c2b182bfd29415d20
Adam Peace <adam.n.peace@gmail.com>
2019-04-10 16:23:01 +0100

    Updates

---
+++ precache-manifest.e8b97c09ae04edfa0566fa23587cd84e.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "27ee0d01ecef6706b147",
+    "url": "/myacademy-oss/static/css/main.e89cca86.chunk.css"
+  },
+  {
+    "revision": "27ee0d01ecef6706b147",
+    "url": "/myacademy-oss/static/js/main.27ee0d01.chunk.js"
+  },
+  {
+    "revision": "eac38007ec8e2361c3ca",
+    "url": "/myacademy-oss/static/js/runtime~main.eac38007.js"
+  },
+  {
+    "revision": "e338d51a9a7ae63b10be",
+    "url": "/myacademy-oss/static/js/2.e338d51a.chunk.js"
+  },
+  {
+    "revision": "32dab0f3b978bedfc2265bca9cb14dbb",
+    "url": "/myacademy-oss/static/media/loading.32dab0f3.svg"
+  },
+  {
+    "revision": "3d5d66de19ba86dc741ffb8915ae0ec3",
+    "url": "/myacademy-oss/static/media/flexible.3d5d66de.png"
+  },
+  {
+    "revision": "74aadcbb9750b88a9b73dd2752745beb",
+    "url": "/myacademy-oss/static/media/accessible.74aadcbb.png"
+  },
+  {
+    "revision": "c5e9d3ca878122f8c0227a855629aab0",
+    "url": "/myacademy-oss/static/media/educational.c5e9d3ca.png"
+  },
+  {
+    "revision": "96aa14c52fc7b857d1d093834ac524ce",
+    "url": "/myacademy-oss/static/media/extensible.96aa14c5.png"
+  },
+  {
+    "revision": "512e9096557dc90e36a513dfc2701212",
+    "url": "/myacademy-oss/static/media/partner_1.512e9096.svg"
+  },
+  {
+    "revision": "0c3039b90440e272ab1e6dd37e24de02",
+    "url": "/myacademy-oss/static/media/partner_2.0c3039b9.svg"
+  },
+  {
+    "revision": "588c39da6a4dc7bb73bcfceabe932e22",
+    "url": "/myacademy-oss/static/media/text_white.588c39da.png"
+  },
+  {
+    "revision": "144db654437dbfe67c453226b45930cf",
+    "url": "/myacademy-oss/index.html"
+  }
+];
