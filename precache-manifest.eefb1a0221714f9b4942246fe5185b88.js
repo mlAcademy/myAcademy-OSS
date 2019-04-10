5206012c3632afa5e784eee99859df0433885e8e
Adam Peace <adam.n.peace@gmail.com>
2019-04-10 16:36:05 +0100

    Updates

---
+++ precache-manifest.eefb1a0221714f9b4942246fe5185b88.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "c816c7a8a79d1d11bf34",
+    "url": "/myAcademy-OSS/static/css/main.e89cca86.chunk.css"
+  },
+  {
+    "revision": "c816c7a8a79d1d11bf34",
+    "url": "/myAcademy-OSS/static/js/main.c816c7a8.chunk.js"
+  },
+  {
+    "revision": "ca5b19e0bfe3b3a046e7",
+    "url": "/myAcademy-OSS/static/js/runtime~main.ca5b19e0.js"
+  },
+  {
+    "revision": "b3a8b8aa974ec0faedcf",
+    "url": "/myAcademy-OSS/static/js/2.b3a8b8aa.chunk.js"
+  },
+  {
+    "revision": "32dab0f3b978bedfc2265bca9cb14dbb",
+    "url": "/myAcademy-OSS/static/media/loading.32dab0f3.svg"
+  },
+  {
+    "revision": "3d5d66de19ba86dc741ffb8915ae0ec3",
+    "url": "/myAcademy-OSS/static/media/flexible.3d5d66de.png"
+  },
+  {
+    "revision": "74aadcbb9750b88a9b73dd2752745beb",
+    "url": "/myAcademy-OSS/static/media/accessible.74aadcbb.png"
+  },
+  {
+    "revision": "c5e9d3ca878122f8c0227a855629aab0",
+    "url": "/myAcademy-OSS/static/media/educational.c5e9d3ca.png"
+  },
+  {
+    "revision": "96aa14c52fc7b857d1d093834ac524ce",
+    "url": "/myAcademy-OSS/static/media/extensible.96aa14c5.png"
+  },
+  {
+    "revision": "512e9096557dc90e36a513dfc2701212",
+    "url": "/myAcademy-OSS/static/media/partner_1.512e9096.svg"
+  },
+  {
+    "revision": "0c3039b90440e272ab1e6dd37e24de02",
+    "url": "/myAcademy-OSS/static/media/partner_2.0c3039b9.svg"
+  },
+  {
+    "revision": "588c39da6a4dc7bb73bcfceabe932e22",
+    "url": "/myAcademy-OSS/static/media/text_white.588c39da.png"
+  },
+  {
+    "revision": "e76d03ca2c0e44e1d8d57ab1e82fb752",
+    "url": "/myAcademy-OSS/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/myacademy-oss/precache-manifest.a8acfcb6fe272eecdb9cd11e702a20ac.js"
+  "/myAcademy-OSS/precache-manifest.eefb1a0221714f9b4942246fe5185b88.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/myacademy-oss/index.html", {
+workbox.routing.registerNavigationRoute("/myAcademy-OSS/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
